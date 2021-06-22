0183f70f120d7b7e51ea7d109ae4bdad8e411a95
KD1801 <kartikdhawan1801@gmail.com>
2021-06-22 14:42:07 +0530

    Added 10-1

--- lab-10-1.sh
+++ lab-10-1.sh
@@ -1,15 +1,17 @@
 arr=( ankit utsav rishab ritu pooja)
-echo "to print all element:"
+echo "To print all element:"
 echo ${arr[@]}
 echo ${arr[*]}
 echo ${arr[*]:0}
 echo ${arr[@]:0}
-
-echo "to print particular element"
+echo ""
+echo "To print particular element"
 echo ${arr[3]}
 echo ${arr[1]}
-
-echo "to print array in range"
+echo ""
+echo "To print array in range"
 echo ${arr[*]:1:4}
 echo ${arr[@]:2:3}
+echo ""
 echo ${arr[0]:2:4}
+#this give kit as 2,3,4 character of ankit = kit
