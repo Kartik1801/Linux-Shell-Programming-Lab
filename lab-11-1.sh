398c015aa91db5521b8ac0635dfbc379332f90be
kartik1801 <kartikdhawan1801@gmail.com>
2021-06-28 13:59:10 +0530

    Average of array

---
+++ lab-11-1.sh
@@ -0,0 +1,15 @@
+echo "Enter No of Element"
+read length
+echo "Enter elements:"
+for ((i=0;i<length;i++))
+do 
+    read num[$i]
+done
+sum=0
+avg=0
+for ((i=0;i<length;i++))
+do 
+    sum=$(($sum+${num[$i]}))
+done
+avg=$(echo "scale=2; $sum / $length" | bc -l)
+echo "Average = "$avg
