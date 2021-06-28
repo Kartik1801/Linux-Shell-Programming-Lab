36a2f72d903c87a7a05617c2defbb09e7c75c0f7
kartik1801 <kartikdhawan1801@gmail.com>
2021-06-28 14:12:08 +0530

    continue statement

--- lab-11-4.sh
+++ lab-11-4.sh
@@ -4,9 +4,9 @@ for num in $nums
 do
  Q=$((num%2))
  if [ $Q -eq 0 ]
-  then 
-   echo "number is even"
-   continue
-  fi
+    then 
+     echo "number is even"
+    continue
+ fi
  echo "number is odd"  
 done
